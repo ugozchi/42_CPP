adc6b76b56ecff0ad220d48b9b4709f65d470ed4
ugozchi <ugozanchi5@gmail.com>
2025-03-13 18:20:28 +0100

    ex02

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -3,5 +3,9 @@
 int main(void)
 {
     std::string str = "HI THIS IS BRAIN";
-    
+    std::string *ptr = &str;
+    std::string &ref = str;
+
+    std::cout << &str << std::endl << ptr << std::endl << &ref << std::endl << std::endl;
+    std::cout << str << std::endl << *ptr << std::endl << ref << std::endl << std::endl;
 }
