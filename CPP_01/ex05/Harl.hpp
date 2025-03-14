abd85430eeaf8c4829391a1da2a870e5ea851664
ugozchi <ugozanchi5@gmail.com>
2025-03-14 14:56:12 +0100

    ex05

---
+++ CPP_01/ex05/Harl.hpp
@@ -0,0 +1,22 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+private:
+    void    debug( void );
+    void    info( void );
+    void    warning( void );
+    void    error( void );
+
+public:
+    Harl();
+    ~Harl();
+
+    void    complain( const std::string& level );
+    void    (Harl::*getFunction( const std::string& level ))();
+};
+
+#endif
