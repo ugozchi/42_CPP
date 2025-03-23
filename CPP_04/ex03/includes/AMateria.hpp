df429fbd966bbaf1b836300659c4fd82d9922f76
ugozchi <ugozanchi5@gmail.com>
2025-03-23 13:28:07 +0100

    start ex03

---
+++ CPP_04/ex03/includes/AMateria.hpp
@@ -0,0 +1,25 @@
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+# include <iostream>
+class ICharacter;
+
+
+class AMateria
+{
+protected:
+    std::string     _type;
+public:
+    AMateria( void );
+    AMateria( std::string const & type );
+    AMateria( const AMateria& src );
+    AMateria&   operator=( const AMateria& src );
+    virtual ~AMateria();
+
+    const std::string&  getType( void ) const;
+
+    virtual AMateria*   clone() const = 0;
+    virtual void        use(ICharacter& target);
+};
+
+#endif
